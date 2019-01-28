117cc28beb2af457e8ff2ea61ab245edc2b5aafb
Kyle <kylebeveridge13@gmail.com>
2019-01-28 13:43:30 -0800

    stashing

--- gameengine.js
+++ gameengine.js
@@ -52,49 +52,6 @@ GameEngine.prototype.startInput = function () {
 
     var that = this;
 
-    // event listeners are added here
-
-    // this.ctx.canvas.addEventListener("click", function (e) {
-    //     that.click = getXandY(e);
-    //     console.log(e);
-    //     console.log("Left Click Event - X,Y " + e.clientX + ", " + e.clientY);
-    // }, false);
-
-    // this.ctx.canvas.addEventListener("contextmenu", function (e) {
-    //     that.click = getXandY(e);
-    //     console.log(e);
-    //     console.log("Right Click Event - X,Y " + e.clientX + ", " + e.clientY);
-    //     e.preventDefault();
-    // }, false);
-
-    // this.ctx.canvas.addEventListener("mousemove", function (e) {
-    //     //console.log(e);
-    //     that.mouse = getXandY(e);
-    // }, false);
-
-    // this.ctx.canvas.addEventListener("mousewheel", function (e) {
-    //     console.log(e);
-    //     that.wheel = e;
-    //     console.log("Click Event - X,Y " + e.clientX + ", " + e.clientY + " Delta " + e.deltaY);
-    // }, false);
-
-    // this.ctx.canvas.addEventListener("keydown", function (e) {
-    //     console.log(e);
-    //     console.log("Key Down Event - Char " + e.code + " Code " + e.keyCode);
-    // }, false);
-
-    // this.ctx.canvas.addEventListener("keypress", function (e) {
-    //     if (e.code === "KeyD") that.d = true;
-    //     that.chars[e.code] = true;
-    //     console.log(e);
-    //     console.log("Key Pressed Event - Char " + e.charCode + " Code " + e.keyCode);
-    // }, false);
-
-    // this.ctx.canvas.addEventListener("keyup", function (e) {
-    //     console.log(e);
-    //     console.log("Key Up Event - Char " + e.code + " Code " + e.keyCode);
-    // }, false);
-
     this.ctx.canvas.addEventListener("keydown", (e) => {
         if(e.keyCode === 32){
             that.space = true;
